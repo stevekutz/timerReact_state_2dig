b2064c799717697386f3b5d3777a130fec2ed414
stevekutz <stkutz@gmail.com>
2021-03-05 16:10:31 -0600

    setting up dropdown options menu

--- app/src/comp/MenuBar.js
+++ app/src/comp/MenuBar.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import burger20 from '../assets/img/Hamburger_icon20.png';
+// import burger2 from '../assets/img/Hamburger_icon_2.jpg';
+// import burger3 from '../assets/img/Hamburger_icon_3.jpg';
+// import burger4 from '../assets/img/Hamburger_icon_4.jpg';
+
 
 import {
     MenuBarContainer,
-
+    IconImageContainer,
+    IconImage,
 
 } from '../styled/menu_bar_style';
 
@@ -11,6 +17,9 @@ const MenuBar = (props) => {
 
     return (
         <MenuBarContainer>
+            <IconImageContainer>
+                <IconImage src = {burger20}/>
+            </IconImageContainer>
 
         </MenuBarContainer>
     )
--- app/src/styled/menu_bar_style.js
+++ app/src/styled/menu_bar_style.js
@@ -4,9 +4,28 @@ import styled from 'styled-components';
 const MenuBarContainer = styled.div`
     border: 1px solid grey;
 
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
     max-width: 100vw;
-    height: 50px;
+    height: 35px;
     background: #111B20;
+    
+    z-index: 10;
+`
+
+const IconImageContainer = styled.div`
+    cursor: pointer;
+    height: 26px;
+    weight: 26px;
+    margin: 5px;
+`
+
+const IconImage = styled.img`
+    color: white;
+    height: 25px;
+    width: 25px;
+    z-indez: 0;
 `
 
 
@@ -18,6 +37,7 @@ const MenuBarOptions = styled.div`
 
 export {
     MenuBarContainer,
-
+    IconImageContainer,
+    IconImage,
 
 };
